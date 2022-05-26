dcce5c48ac6f53157c2f42914790abe3e0027c27
Haider Ali <coderali060@gmail.com>
2022-05-26 09:29:23 +0500

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showmenu() {
+    var m = document.querySelector('.menu');
+    m.classList.add('.smenu');
+}
+
+function closemenu() {
+    var m = document.querySelector('.menu');
+    m.classList.remove('.smenu');
+}
